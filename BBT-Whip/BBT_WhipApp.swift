aee6816c010ef0f8e23c0df0e2d6ba7c42dd7082
nlhomme <9362475+nlhomme@users.noreply.github.com>
2020-11-06 22:14:02 +0100

    Initial Commit

---
+++ BBT-Whip/BBT_WhipApp.swift
@@ -0,0 +1,17 @@
+//
+//  BBT_WhipApp.swift
+//  BBT-Whip
+//
+//  Created by Nicolas Lhomme on 06/11/2020.
+//
+
+import SwiftUI
+
+@main
+struct BBT_WhipApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ BBT-Whip/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  BBT-Whip
+//
+//  Created by Nicolas Lhomme on 06/11/2020.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
