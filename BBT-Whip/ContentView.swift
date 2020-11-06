3a35f6e12587bf54115dcfc7b437a18b5f21bf89
nlhomme <9362475+nlhomme@users.noreply.github.com>
2020-11-07 00:56:42 +0100

    V1 OK

--- BBT-Whip/ContentView.swift
+++ BBT-Whip/ContentView.swift
@@ -6,16 +6,42 @@
 //
 
 import SwiftUI
+import AVFoundation
+
+let path = Bundle.main.path(forResource: "whip-cracking.mp3", ofType: nil)!
+let url = URL(fileURLWithPath: path)
+
+var whipCrackingSoundEffect: AVAudioPlayer?
 
 struct ContentView: View {
     var body: some View {
-        Text("Hello, world!")
-            .padding()
+        ZStack {
+            ShakableViewRepresentable()
+                .allowsHitTesting(/*@START_MENU_TOKEN@*/false/*@END_MENU_TOKEN@*/)
+            
+            VStack{
+                Spacer()
+                Image("Whip")
+                    .resizable()
+                    .scaledToFit()
+                Spacer()
+            }
+        }
+            .onReceive(messagePublisher){ _ in
+                do {
+                    whipCrackingSoundEffect = try AVAudioPlayer(contentsOf: url)
+                    whipCrackingSoundEffect?.play()
+                } catch {
+                    // couldn't load a file :(
+                }
+        }
     }
 }
 
 struct ContentView_Previews: PreviewProvider {
     static var previews: some View {
         ContentView()
+            .preferredColorScheme(.dark)
     }
 }
+
---
+++ BBT-Whip/ShakeGestureManager.swift
@@ -0,0 +1,29 @@
+//
+//  ShakeGestureManager.swift
+//  BBT-Whip
+//
+//  Created by Nicolas Lhomme on 06/11/2020.
+//
+
+import SwiftUI
+import Combine
+
+let messagePublisher = PassthroughSubject<Void, Never>()
+
+class ShakableViewController: UIViewController {
+    
+    override func motionBegan(_ motion: UIEvent.EventSubtype, with event: UIEvent?) {
+        guard motion == .motionShake else { return }
+        messagePublisher.send()
+    }
+}
+
+struct ShakableViewRepresentable: UIViewControllerRepresentable {
+    
+    func makeUIViewController(context: Context) -> ShakableViewController {
+        ShakableViewController()
+    }
+    func updateUIViewController(_ uiViewController: ShakableViewController, context: Context) {
+        
+    }
+}
